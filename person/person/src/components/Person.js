048828fccbe41cbb8285f152794330276faaebc2
HenrySpock <motiviquestudio@gmail.com>
2023-08-16 01:22:39 -0500

    Final

--- person/person/src/components/Person.js
+++ person/person/src/components/Person.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-
-// function Person(props) {
-//     let nameDisplay = props.name.length > 8 ? props.name.substring(0, 6) + "..." : props.name;
-
-//     return (
-//         <div className="person">
-//             <p>Learn some information about this person</p>
-//             <p>Name: {nameDisplay}</p>
-//             <p>Age: {props.age}</p>
-
-//             {props.age >= 18 ? (
-//                 <h3>please go vote!</h3>
-//             ) : (
-//                 <h3>you must be 18</h3>
-//             )}
-
-//             <ul>
-//                 {props.hobbies.map(hobby => (
-//                     <li key={hobby}>{hobby}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default Person;
-
 import React from 'react';
 
 function Person(props) {
